e4653eba843d3ec51b7b495de70c5ee8a0600b34
casellner <114782256+casellner@users.noreply.github.com>
2025-06-01 15:52:58 -0400

    Remove unreachable break statements

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,19 +22,16 @@ export function fdate(timestamp: string): string {
     hour12: true
   };
 
+  // Return formatted date
   switch (settings['ui/date_format']) {
     case "MM/DD/YYYY":
       return date.toLocaleString('en-US', options); // ex. April 21, 2025 at 11:53:53 PM
-      break;
     case "DD/MM/YYYY":
       return date.toLocaleString('en-GB', options); // ex. 21 April 2025 at 11:53:53 pm
-      break;
     case "YYYY-MM-DD":
       return date.toISOString(); // ex. 2025-04-22T03:53:53.941Z
-      break;
     default:
       return date.toLocaleString('en-US', options);
-      break;
   }
 }
 
